c2dc082ef0bb8e42dbedc542858d1d26db5fec75
AsifHossain22 <only1asif4you22@gmail.com>
2025-09-05 23:31:34 +0400

    Module 39-6 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import Counter from "./Counter";
 import Batsman from "./Batsman";
+import Users from "./Users";
+import Friends from "./Friends";
 import "./App.css";
+import { Suspense } from "react";
+
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json()
+);
+
+const fetchFriends = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
 
 function App() {
+  const friendsPromise = fetchFriends();
+
   function handleClick() {
     alert("I'm clicked");
   }
@@ -19,6 +33,14 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+
+      {/* <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense> */}
+
+      <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
 
       <Batsman></Batsman>
       <Counter></Counter>
